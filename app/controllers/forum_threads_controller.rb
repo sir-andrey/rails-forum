c5224b350e91492f2c23e0c3066bb190c8b17bc5
sir-andrey <maco231455@gmail.com>
2020-09-23 16:43:54 +0700

    Thread Validation

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -14,8 +14,12 @@ class ForumThreadsController < ApplicationController
   def create
   	@thread = ForumThread.new(resource_params)
   	@thread.user = User.first
-  	@thread.save
-  	redirect_to root_path
+
+  	if @thread.save
+  		redirect_to root_path
+  	else
+  		render 'new'
+  	end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'forum_threads/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "forum_threads#index"
 
-  resources :forum_threads, only: [:show, :new, :create]
+  resources :forum_threads, only: [:index, :show, :new, :create]
 end
